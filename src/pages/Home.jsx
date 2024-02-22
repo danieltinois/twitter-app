c6ef19f784d43c4ea8c2d834ea4c47d203ed60d0
danieltinois <danieltinois@gmail.com>
2024-02-22 00:34:00 -0300

    added dark theme and updated routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,10 @@ import { auth } from "../services/firebase";
 import { getDatabase, ref, push, onValue } from "firebase/database";
 import classNames from "classnames";
 
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+
 export const Home = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const [text, setText] = useState("");
   const [user, setUser] = useState(null);
   const [email, setEmail] = useState("");
@@ -76,14 +79,38 @@ export const Home = () => {
     }
   }
 
+  function toggleTheme() {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  }
+
   if (!user) return null;
 
   return (
     <App>
-      <header className="flex justify-between items-center py-3 px-6 bg-white border-b border-gray-200">
-        <h1 className="text-sky-500 text-xl font-bold">aluritter</h1>
+      <header
+        className={classNames(
+          "flex justify-between items-center py-3 px-6 border-b border-gray-200",
+          {
+            "bg-gray-800": isDarkTheme,
+            "border-gray-700": isDarkTheme,
+            "shadow-lg": isDarkTheme,
+          }
+        )}
+      >
+        <div className="flex items-center">
+          <h1
+            className="text-xl font-bold pr-4 text-sky-500"
+          >
+            Simple Twitter
+          </h1>
+          <button onClick={toggleTheme}>
+            {isDarkTheme ? <MdLightMode color="#FFFFCC" size={20} /> : <MdDarkMode color="#065F96" size={20} />}
+          </button>
+        </div>
         <div className="flex items-center space-x-4">
-          <span>{email}</span>
+          <span className={classNames("", { "text-white": isDarkTheme })}>
+            {email}
+          </span>
           <button
             onClick={handleSignOut}
             className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
@@ -92,50 +119,75 @@ export const Home = () => {
           </button>
         </div>
       </header>
-      <main className="bg-gray-100 min-h-screen flex justify-center">
-        <div className="m-16 w-2/3">
-          <label className="block mb-2 pl-2">Alurite agora mesmo</label>
-          <textarea
-            className="w-full h-36 border border-gray-300 rounded-md px-4 py-2 mb-4 shadow-md"
-            value={text}
-            onChange={handleChangeCaracteres}
-            maxLength="255"
-            style={{ resize: "none" }}
-            placeholder="Sem ideias? Que tal escrever como foi seu dia hoje..."
-          />
-
-          <div className="flex justify-between">
-            <span className="text-emerald-500">
-              Você ainda pode digitar {textLimit - text.length} caracteres
-            </span>
-            <button
-              onClick={handleAddTweet}
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+      <main
+        className={classNames("flex justify-center items-center", {
+          "bg-gray-100": !isDarkTheme,
+          "bg-gray-900": isDarkTheme,
+        })}
+      >
+        <div className="m-16 w-2/3 ">
+          <div className={classNames("App", { dark: isDarkTheme })}>
+            <label
+              className={classNames("block mb-2 pl-2", {
+                "text-white": isDarkTheme,
+              })}
             >
-              aluritar
-            </button>
-          </div>
-
-          {tweets.map((tweet) => (
-            <div
-              key={tweet.id}
+              Faça um tweet agora mesmo
+            </label>
+            <textarea
               className={classNames(
-                "w-full bg-white mt-8 mb-8 pl-4 pr-4 flex flex-col rounded-md shadow-md"
+                "w-full h-36 border border-gray-300 rounded-md px-4 py-2 mb-4 shadow-md",
+                {
+                  "border-gray-700": isDarkTheme,
+                  "bg-gray-800": isDarkTheme,
+                  "shadow-lg": isDarkTheme,
+                  "text-white": isDarkTheme,
+                }
               )}
-              style={{ maxWidth: "100%" }} // Defina o maxWidth conforme necessário
-            >
-              <h1 className="text-lg pt-5 mb-7">{tweet.text}</h1>
+              value={text}
+              onChange={handleChangeCaracteres}
+              maxLength="255"
+              style={{ resize: "none" }}
+              placeholder="Sem ideias? Que tal escrever como foi seu dia hoje..."
+            />
 
-              <div className="flex justify-between items-center mt-auto pb-3">
-                <span className="text-sky-500">{tweet.user}</span>
-                <span className="opacity-40 pr-4 text-sm">
-                  {new Date(tweet.timestamp).toLocaleString("pt-BR", {
-                    hour12: false,
-                  })}
-                </span>
-              </div>
+            <div className="flex justify-between">
+              <span className="text-emerald-500">
+                Você ainda pode digitar {textLimit - text.length} caracteres
+              </span>
+              <button
+                onClick={handleAddTweet}
+                className="bg-sky-500 hover:bg-sky-600 text-white px-4 py-2 rounded-md"
+              >
+                aluritar
+              </button>
             </div>
-          ))}
+
+            {tweets.map((tweet) => (
+              <div
+                key={tweet.id}
+                className={classNames(
+                  "w-full mt-8 mb-8 pl-4 pr-4 flex flex-col rounded-md",
+                  {
+                    "bg-white shadow-md": !isDarkTheme,
+                    "bg-gray-800 text-white shadow-lg": isDarkTheme,
+                  }
+                )}
+                style={{ maxWidth: "100%" }} // Defina o maxWidth conforme necessário
+              >
+                <h1 className="text-lg pt-5 mb-7">{tweet.text}</h1>
+
+                <div className="flex justify-between items-center mt-auto pb-3">
+                  <span className="text-sky-500">{tweet.user}</span>
+                  <span className="opacity-40 pr-4 text-sm">
+                    {new Date(tweet.timestamp).toLocaleString("pt-BR", {
+                      hour12: false,
+                    })}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </App>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,31 @@
 //teste
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Home } from "./Home";
 import { NotFound } from "./NotFound";
 import { SignUp } from "./SignUp";
 import { SignIn } from "./SignIn";
 
-export const Pages = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  </Router>
-);
+export const Pages = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
