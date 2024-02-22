21cf7ee6064f677eb4ea1e6cd3f87338ce127957
danieltinois <danieltinois@gmail.com>
2024-02-22 03:44:20 -0300

    updated css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -221,7 +221,7 @@ export const Home = () => {
                 </div>
               )}
             </div>
-            <div className="flex justify-between px-2 pr-6">
+            <div className="flex justify-between pr-2">
               <span className="text-emerald-500">
                 Você ainda pode digitar {textLimit - text.length} caracteres
               </span>
